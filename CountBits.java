2e264d1c2e62ffb3afe64b1ec6969b57f1403805
Priyaa-11 <priyakinnu18@gmail.com>
2026-01-02 16:05:40 +0530

    Add files via upload

---
+++ CountBits.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+class CountBits{
+    static int CountBits(int item){
+        if(item==0){
+            return 1;
+        }
+        else{
+            return (int)(Math.log(item)/Math.log(2)+1);
+        }
+    }
+    public static void main(String[] args) throws IOException{
+        Scanner sc = new Scanner(System.in);
+        int count=0;
+        String strNums = sc.nextLine();
+        String[] tokens = strNums.split(" ");
+        int[] ary = new int[tokens.length];
+        int i =0;
+        for(i=0;i<tokens.length;++i){
+            ary[i]=Integer.parseInt(tokens[i]);
+        }
+        for(i=0;i<ary.length;i++){
+            count+=CountBits(ary[i]);
+        }
+        System.out.println(count);
+    }
+}
